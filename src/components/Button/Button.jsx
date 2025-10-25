1669fc158c48abd17f5c4fd23ec7059727aaa6fb
Marcelo <jmlporto1993@gmail.com>
2025-10-25 17:44:04 -0300

    2

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import "./Button.css";
 
 const GenericButton = ({ texto, acao }) => {
   return (
-    <button onClick={() => acao()} className="g-button">
+    <button onClick={() => acao()} className="g-button" id="button">
       {texto}
     </button>
   );
